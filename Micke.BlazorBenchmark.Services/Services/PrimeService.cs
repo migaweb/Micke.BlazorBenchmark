32fe9dedd1f2d57f464ee1be6f0718bf9bdf7f06
Mikael Storman Gustavsson <miga02@gmail.com>
2021-11-13 13:32:11 +0100

    Added searching for prime numbers test.

---
+++ Micke.BlazorBenchmark.Services/Services/PrimeService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Micke.BlazorBenchmark.Services.Services
+{
+  public class PrimeService
+  {
+    public List<int> FindPrimeNumbersUpTo(int end)
+    {
+      var start = 0;
+
+      return
+        Enumerable.Range(start, end - start)
+            .Where(IsPrime)
+            .Select(number => number)
+            .ToList();
+    }
+    private bool IsPrime(int number)
+    {
+      bool CalculatePrime(int value)
+      {
+        var possibleFactors = Math.Sqrt(number);
+
+        for (var factor = 2; factor <= possibleFactors; factor++)
+        {
+          if (value % factor == 0)
+          {
+            return false;
+          }
+        }
+
+        return true;
+      }
+
+      return number > 1 && CalculatePrime(number);
+    }
+  }
+}
--- Micke.BlazorBenchmark.WASM/Client/Program.cs
+++ Micke.BlazorBenchmark.WASM/Client/Program.cs
@@ -4,13 +4,9 @@ using Micke.BlazorBenchmark.Services.Services;
 using Micke.BlazorBenchmark.WASM.Shared.Contracts;
 using Micke.BlazorBenchmark.WASM.Shared.Entities;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Micke.BlazorBenchmark.WASM.Client
@@ -25,6 +21,7 @@ namespace Micke.BlazorBenchmark.WASM.Client
       builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
       builder.Services.AddScoped<IPersistenceService, IndexedDB>();
       builder.Services.AddScoped<IExcelReader<Article>, ArticleExcelReader>();
+      builder.Services.AddScoped<PrimeService>();
       builder.Services.AddScoped<BusyOverlayService>();
 
       await builder.Build().RunAsync();
